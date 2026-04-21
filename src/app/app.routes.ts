baaec79de9e30d67f3ae6df9b97dc949db11bebf
PriyanshiLakhani <priyanshi.lakhani.it@gmail.com>
2026-04-21 14:55:55 +0530

    fix : preline

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./home/home').then((mod) => mod.Home),
+    data: {
+      title: 'Prompt - Tailwind CSS Multipurpose One Page Landing Template',
+    },
+  },
+
+]
