14c65d63b28336552aa9ed70360a2e7a9c217c0a
shruvaghani <shrutivaghani2004@gmail.com>
2026-04-23 10:42:01 +0530

    back to top

--- src/app/layout/admin-layout/admin-layout.ts
+++ src/app/layout/admin-layout/admin-layout.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { AdminHeader } from "@app/components/admin-header/admin-header";
 import { RouterOutlet } from '@angular/router';
 import { AdminFooter } from '@app/components/admin-footer/admin-footer';
+import { BackToTop } from "@app/components/back-to-top/back-to-top";
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [AdminHeader, RouterOutlet, AdminFooter],
+  imports: [AdminHeader, RouterOutlet, AdminFooter, BackToTop],
   templateUrl: './admin-layout.html',
   styles: ``,
 })
