a3c5319e0abed0d203f6540319a44e4670dfce31
PriyanshiLakhani <priyanshi.lakhani.it@gmail.com>
2026-04-24 13:34:33 +0530

    fix : mobile-nav

--- src/app/components/mobile-nav/mobile-nav.ts
+++ src/app/components/mobile-nav/mobile-nav.ts
@@ -1,7 +1,8 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
 import { MENU_DATA } from './data';
 
+declare var HSOverlay: any;
 @Component({
   selector: 'app-mobile-nav',
   imports: [RouterLink],
@@ -9,7 +10,22 @@ import { MENU_DATA } from './data';
   styles: ``,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-  
 export class MobileNav {
   MENU_DATA = MENU_DATA;
+  closeMenu() {
+    const overlay = document.getElementById('mobileMenu');
+
+    if (overlay) {
+      // close panel
+      overlay.classList.add('hidden');
+      overlay.classList.remove('hs-overlay-open');
+      overlay.classList.remove('translate-x-0');
+    }
+
+    // remove body scroll lock
+    document.body.classList.remove('hs-overlay-body-open');
+
+    // 🔥 remove ALL backdrops (important)
+    document.querySelectorAll('.hs-overlay-backdrop').forEach((el) => el.remove());
+  }
 }
